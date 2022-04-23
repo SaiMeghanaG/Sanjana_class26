0cee9a7582727a26b84f3a89ea5b749c5144cfe3
SaimeghanaG <garudadrisaimeghana@gmail.com>
2022-04-23 11:41:19 +0530

    Added_Ground

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg, ground;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
